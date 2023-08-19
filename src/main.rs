55e81590099482102224e246011baf09fb1c2d54
One <43485962+c-git@users.noreply.github.com>
2023-08-19 12:05:05 -0400

    Create MRE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    match File::open("NON-EXISTENT") {    
+        Ok(_) => {   
+            "anything".to_string()
+        }
+        Err(_) => format!("1234567890+1234567890+1234567890+1234567890+1234567890+1234567890+1234567890+123456789"),
+    };
+    // If the text inside of the format string is 1 char shorter the problem goes away
 }
